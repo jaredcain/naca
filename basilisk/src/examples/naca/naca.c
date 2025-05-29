81ff21409da85d150ef3754b50246e263c4a0918
Jared Cain <jared.j.cain@gmail.com>
2025-05-29 11:21:21 -0400

    Update naca.c

--- basilisk/src/examples/naca/naca.c
+++ basilisk/src/examples/naca/naca.c
@@ -1,16 +1,21 @@
-// qcc -autolink -Wall -O2 naca.c -o naca -lm -lfb_tiny
-// ./naca <NACA> <AoA> <<Re>><<LEVEL>>
+// CC99='mpicc -std=c99' qcc -autolink -Wall -O2 -D_MPI=1 naca.c -o naca -lm -lfb_tiny
+// mpirun -np 4 ./naca <<NACA>> <<AoA>> <<Re>> <<LEVEL>>
+// tail -f 0012_2.log
+// http://basilisk.fr/three.js/editor/index.html?ws://COMECH-2065.ds.sc.edu:7100
 
 #include "embed.h"
 #include "navier-stokes/centered.h"
+#include "navier-stokes/perfs.h"
 #include "view.h"
+#include <mpi.h>
 
 double mm=0., pp=0., tt=0.12; // camber,location,thickness
 double Reynolds = 10000;
 double aoa = 0. * M_PI / 180.0;
+const double t_end = 30;
 
-const char* nacaset;
-int maxlevel = 11;
+const char* nacaset="0012";
+int maxlevel = 13;
 face vector muv[];
 
 #define chord   (1.)
@@ -27,15 +32,18 @@ void nacaset_f(const char* nacaset, double* mm, double* pp, double* tt)
 
 int main(int argc, char *argv[])
 {
-  nacaset = argv[1];
-  aoa = atof(argv[2]) * M_PI / 180.0;
-  if (argc > 3) {
-    Reynolds = atof(argv[3]);
-    if (argc > 4) {
-      maxlevel = atoi(argv[4]);
-    }
+  if (argc > 1) {
+	  nacaset = argv[1];
+	  if (argc > 2) {
+	  aoa = atof(argv[2]) * M_PI / 180.0;
+		  if (argc > 3) {
+				Reynolds = atof(argv[3]);
+				if (argc > 4) {
+				  maxlevel = atoi(argv[4]);
+				}
+		    }
+	    }
   }
-  
   nacaset_f(nacaset, &mm, &pp, &tt);
   
   char log_filename[50];
@@ -43,8 +51,9 @@ int main(int argc, char *argv[])
   freopen(log_filename, "w", stderr);
   
   L0 = 16.;
-  origin (-L0/2, -L0/2.);
-  N = 1 << maxlevel;
+  origin (-L0/8, -L0/2.);
+  N = 1 << (maxlevel-4);
+  TOLERANCE = 1e-4;
   mu = muv;
   run(); 
 }
@@ -96,12 +105,22 @@ double naca(double x, double y, double mm, double pp, double tt)
 
 event init (t = 0)
 {
-  solid (cs, fs, naca(x,y,mm,pp,tt));
+  scalar csf[];
+  astats ss;
+  int ic = 0;
+  do {
+    ic++;
+    solid (cs, fs, naca(x, y, mm, pp, tt));
+    foreach()
+      csf[] = cs[];
+    ss = adapt_wavelet ({csf}, (double[]) {1.e-30},
+			maxlevel, minlevel = (maxlevel-4));
+  } while ((ss.nf || ss.nc) && ic < 100);
   foreach()
     u.x[] =  1.;
 }
 
-event logfile (i += 10){
+event logfile (i++; t <= t_end) {
   scalar omega[];
   vorticity (u,omega);
   face vector muv[];
@@ -124,15 +143,16 @@ event logfile (i += 10){
   
   stats om  = statsf(omega);
   fprintf (stderr, "%d %g %g %g %g\n", i, t, om.max, CL, CD);
+  fflush(stderr);
 }
 
-event movies (t += 0.05; t <= 30.)
+event movies (t += 0.05; t <= t_end)
 {
   scalar omega[],m[];
   vorticity(u, omega);
   view(quat = {0.000, 0.000, 0.000, 1.000}, fov = 30, near = 0.01, far = 1000, tx = -0.0, ty = 0.05, tz = -2.25, width = 512, height = 512);
   box();
-  squares(color = "omega"); //for some reason freaks out and is a color bomb with out this one
+  squares(color = "omega"); //for some reason freaks out and is a color bomb without this one
   squares(color = "omega", spread = -1, cbar = true, border = true, pos = {-0.725, 0.4}, mid = true, format = " %.0f", levels = 100, size = 17 , lw=1, fsize = 75);
   //cells();
   draw_vof(c="cs",lw=1, lc = {0,0,0});
@@ -149,3 +169,4 @@ event movies (t += 0.05; t <= 30.)
 event adapt (i++) {
   adapt_wavelet ({cs,u}, (double[]){1e-15,1e-3,1e-3}, maxlevel, 7);
 }
+
